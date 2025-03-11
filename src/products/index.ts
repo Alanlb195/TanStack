9cfc440e53c73842a55c852df4ed8bbcf575995e
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-03-11 06:09:47 -0500

    Feat -prefetch and use query added

--- src/products/index.ts
+++ src/products/index.ts
@@ -1,8 +1,14 @@
 
-
+export { productsApi } from './api/productsApi';
 
 export { ProductCard } from './components/ProductCard';
 export { ProductList } from './components/ProductList';
+
+export { useProducts } from './services/hooks/useProducts';
+export { useProduct } from './services/hooks/useProduct';
+export { usePrefetchProduct } from './services/hooks/usePrefetchProduct';
+
+export type { Product } from './interfaces/Product'
 
 
 export { StoreLayout } from './layout/StoreLayout';
@@ -12,3 +18,7 @@ export { CompleteListPage } from './pages/CompleteListPage';
 export { MensPage } from './pages/MensPage';
 export { NewProduct } from './pages/NewProduct';
 export { WomensPage } from './pages/WomensPage';
+export { ProductById } from './pages/ProductById'
+
+
+export * as productActions from './services/actions';
---
+++ src/products/services/actions.tsx
@@ -0,0 +1,32 @@
+import { type Product, productsApi } from "..";
+
+interface GetProductsOptions {
+    filterKey?: string;
+}
+
+const sleep = (seconds: number = 2): Promise<boolean> => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(true);
+        }, seconds * 1000);
+    })
+}
+
+export const getProducts = async ({ filterKey }: GetProductsOptions) => {
+    await sleep(2);
+
+    const filterUrl = (filterKey) ? `category=${filterKey}` : ''
+
+    const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
+
+    return data;
+}
+
+export const getProductById = async (id: number): Promise<Product> => {
+    // await sleep(2);
+
+
+    const { data } = await productsApi.get<Product>(`/products/${id}`);
+
+    return data;
+}
